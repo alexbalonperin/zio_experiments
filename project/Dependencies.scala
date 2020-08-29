8fe10682e351eaf165619449c8a2e55fead6aaab
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-29 15:29:22 +0200

    Update zio-opentracing to 0.7.0 (#58)
    Co-authored-by: Alexandre Balon-Perin <alexbp13@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val logback = "1.2.3"
     val opentracing = "0.33.0"
     val zio = "1.0.1"
-    val zioOpenTracing = "0.6.0"
+    val zioOpenTracing = "0.7.0"
     val zioInteropCats = "2.0.0.0-RC14"
     val zioLogging = "0.4.0"
     val zipkin = "2.15.1"
