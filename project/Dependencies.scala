2a93ee247ffe82cd3006259cace492d4236f03fb
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-29 15:32:06 +0200

    Update zio-interop-cats to 2.1.4.0 (#60)
    Co-authored-by: Alexandre Balon-Perin <alexbp13@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val opentracing = "0.33.0"
     val zio = "1.0.1"
     val zioOpenTracing = "0.7.0"
-    val zioInteropCats = "2.0.0.0-RC14"
+    val zioInteropCats = "2.1.4.0"
     val zioLogging = "0.4.0"
     val zipkin = "2.15.1"
 
