8dd618a00b5b9b0d22fada61a6ccecd20530c98e
Scala Steward <me@scala-steward.org>
2020-12-07 08:04:17 +0100

    Update zipkin-reporter, ... to 2.16.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val zioOpenTracing = "0.7.0"
     val zioInteropCats = "2.2.0.1"
     val zioLogging = "0.5.4"
-    val zipkin = "2.16.1"
+    val zipkin = "2.16.2"
 
     val scalaTest = "3.1.1"
 
